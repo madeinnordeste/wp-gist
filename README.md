8bde79d6888b1501c213e02962d72c8190107a4b
Luiz Alberto S. Ribeiro <madeinnordeste@gmail.com>
2012-06-22 17:13:28 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# WP Gist
+
+Easily embed and share Github gists on your WordPress blog
+
+##Usage:
+
+Paste a gist link into a blog post or page and it will be embedded eg:
+
+    https://gist.github.com/2926827
+
+If a gist has multiple files you can select one using a url in the following format:
+
+    https://gist.github.com/2926827?file=embed-gist.php
+
+Original Post: 
+
+* http://interconnectit.com/3920/embed-all-the-gists/
+* http://www.wprecipes.com/easily-embed-and-share-github-gists-on-your-wordpress-blog/
---
+++ wp-gist.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+Plugin Name: WP-Gist
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: Easily embed and share Github gists on your WordPress blog. Original Post http://interconnectit.com/3920/embed-all-the-gists/
+Version: 1
+Author: Luiz Alberto
+Author URI: https://github.com/madeinnordeste/wp-gist
+*/
+
+/**
+ * Usage:
+ * Paste a gist link into a blog post or page and it will be embedded eg:
+ * https://gist.github.com/2926827
+ *
+ * If a gist has multiple files you can select one using a url in the following format:
+ * https://gist.github.com/2926827?file=embed-gist.php
+ *
+ * Original Post: 
+ * http://interconnectit.com/3920/embed-all-the-gists/
+ */ 
+ 
+wp_embed_register_handler( 'gist', '/https:\/\/gist\.github\.com\/(\d+)(\?file=.*)?/i', 'wp_embed_handler_gist' );
+
+function wp_embed_handler_gist( $matches, $attr, $url, $rawattr ) {
+
+	$embed = sprintf(
+			'<script src="https://gist.github.com/%1$s.js%2$s"></script>',
+			esc_attr($matches[1]),
+			esc_attr($matches[2])
+			);
+
+	return apply_filters( 'embed_gist', $embed, $matches, $attr, $url, $rawattr );
+}
+
+?>
